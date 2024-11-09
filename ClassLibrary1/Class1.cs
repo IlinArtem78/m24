df2bf68d861f881ad8b759473759d8fd2a09d2a9
IlinArtem78 <artem.ilin1994@gmail.com>
2024-11-09 21:32:49 +0300

    first commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,43 @@
+﻿using Npgsql;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        //   Server=адрес_сервера;Database=имя_базы_данных;User Id = логин; Password=пароль;
+
+        public async void Connect()
+        {
+            var connectionString = "Host=localhost;Database=shop;Username=postgres;Password=!QAZ@WSX;";
+
+            
+            
+            try
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+                {
+                    connection.Open();
+                    using (NpgsqlCommand command = connection.CreateCommand())
+                    {
+                        command.CommandText = "SELECT * FROM clients";
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine(reader["CustomerName"]);
+                            }
+                        }
+                    }
+                }
+
+            }
+                catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+
+    }
+}
---
+++ ConnectionApp/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassLibrary1;
+
+
+class Program
+{
+
+ public static void Main(string[] args)
+    {
+        var t = new ClassLibrary1.Class1(); 
+        t.Connect();
+
+    }
+
+/*public async void Connect()
+{
+    var connString = "Host=PostgreSQL;Username=postgres;Password=!QAZ2wsx;Database=shop";
+
+    await using var conn = new NpgsqlConnection(connString);
+    await conn.OpenAsync();
+
+    await using (var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn))
+    {
+        cmd.Parameters.AddWithValue("p", "Hello world");
+        await cmd.ExecuteNonQueryAsync();
+    }
+    await using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+
+    await using (var reader = await cmd.ExecuteReaderAsync())
+    {
+        while (await reader.ReadAsync())
+            Console.WriteLine(reader.GetString(0));
+    }*/
+}
+
